7ddcdd08d3aea9f1128c336085317c3aa1327495
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-09-22 06:23:00 -0700

    Added policy checks for note

--- src/Policy/NotePolicy.php
+++ src/Policy/NotePolicy.php
@@ -13,6 +13,21 @@ class NotePolicy
         return $identity->getIdentifier() === $note->user_id;
     }
 
+    public function canIndex(Identity $identity, Note $note): bool
+    {
+        return $this->identifierIsUserId($identity, $note);
+    }
+
+    public function canView(Identity $identity, Note $note): bool
+    {
+        return $this->identifierIsUserId($identity, $note);
+    }
+
+    public function canAdd(Identity $identity, Note $note): bool
+    {
+        return $this->identifierIsUserId($identity, $note);
+    }
+
     public function canEdit(Identity $identity, Note $note): bool
     {
         return $this->identifierIsUserId($identity, $note);
