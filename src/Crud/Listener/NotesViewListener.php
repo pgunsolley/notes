aa707da9bf53e63f49d41ec269b3c5f7247e0d5e
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-09-25 05:25:18 -0700

    Added relatedModels event handler to NotesViewListener; not implemented

--- src/Crud/Listener/NotesViewListener.php
+++ src/Crud/Listener/NotesViewListener.php
@@ -5,14 +5,28 @@ declare(strict_types=1);
 namespace App\Crud\Listener;
 
 use App\Crud\Listener\IdentityAwareTrait;
+use Cake\Event\EventInterface;
 use Crud\Listener\BaseListener;
 
 class NotesViewListener extends BaseListener
 {
     use IdentityAwareTrait;
 
+    public function implementedEvents(): array
+    {
+        return parent::implementedEvents() + ['Crud.relatedModel' => 'relatedModel'];
+    }
+
     public function startup()
     {
         $this->_action()->setConfig('scaffold.page_title', 'Notes');
+    }
+
+    public function relatedModel(EventInterface $event)
+    {
+        $subject = $event->getSubject();
+        $association = $subject->association;
+        
+        // TODO: Refactor IdentifierAwareTrait
     }
 }
