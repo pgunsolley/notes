ba5d5646240bfccb0ae7de4365ebba567aeab507
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-09-26 10:32:53 -0700

    Modified templates to render only child/parent body

---
+++ templates/element/view/related/has_many.php
@@ -0,0 +1,86 @@
+<?php
+use Cake\Utility\Inflector;
+
+$renderField = fn(string $field) => in_array($field, ['body']);
+
+if (empty($associations['manyToMany'])) {
+    $associations['manyToMany'] = [];
+}
+
+if (empty($associations['oneToMany'])) {
+    $associations['oneToMany'] = [];
+}
+$relations = array_merge($associations['oneToMany'], $associations['manyToMany']);
+
+$i = 0;
+foreach ($relations as $alias => $details) :
+    $otherSingularVar = $details['propertyName'];
+    ?>
+    <div class="related">
+        <h3><?= __d('crud', 'Related {0}', [Inflector::humanize($details['controller'])]); ?></h3>
+        <div class="actions-wrapper mb-3">
+            <?= $this->CrudView->createRelationLink($alias, $details, ['class' => 'btn btn-secondary']);?>
+        </div>
+        <?php
+        if (${$viewVar}->{$details['entities']}) :
+            ?>
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <?php
+                        $otherFields = array_keys(${$viewVar}->{$details['entities']}[0]->toArray());
+                        if (isset($details['with'])) {
+                            $index = array_search($details['with'], $otherFields);
+                            unset($otherFields[$index]);
+                        }
+
+                        foreach ($otherFields as $field) {
+                            if ($renderField($field)) {
+                                echo '<th>' . Inflector::humanize($field) . '</th>';
+                            }
+                        }
+                        ?>
+                        <th class="actions">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $i = 0;
+                    foreach (${$viewVar}->{$details['entities']} as ${$otherSingularVar}) :
+                        ?>
+                        <tr>
+                            <?php
+                            foreach ($otherFields as $field) {
+                                ?>
+                                <?php 
+                                    if ($renderField($field)) {
+                                        echo '<td>' . $this->CrudView->process($field, ${$otherSingularVar}) . '</td>';
+                                    }
+                                ?>
+                                <?php
+                            }
+                            ?>
+                            <td class="actions">
+                                <?= $this->Html->link(__d('crud', 'View'), ['plugin' => $details['plugin'], 'controller' => $details['controller'], 'action' => 'view', ${$otherSingularVar}[$details['primaryKey']]], ['class' => 'btn btn-secondary']); ?>
+                                <?= $this->Html->link(__d('crud', 'Edit'), ['plugin' => $details['plugin'], 'controller' => $details['controller'], 'action' => 'edit', ${$otherSingularVar}[$details['primaryKey']]], ['class' => 'btn btn-secondary']); ?>
+                                <?= $this->Form->postLink(
+                                    __d('crud', 'Delete'),
+                                    ['plugin' => $details['plugin'], 'controller' => $details['controller'], 'action' => 'delete', ${$otherSingularVar}[$details['primaryKey']]],
+                                    [
+                                        'class' => 'btn btn-danger btn-delete',
+                                        'confirm' => __d('crud', 'Are you sure you want to delete this record?'),
+                                        'name' => '_delete',
+                                    ]
+                                ); ?>
+                            </td>
+                        </tr>
+                        <?php
+                    endforeach;
+                    ?>
+                </tbody>
+            </table>
+            <?php
+        endif;
+        ?>
+    </div>
+<?php endforeach; ?>
